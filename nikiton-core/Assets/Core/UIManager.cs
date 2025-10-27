0e2c852661950c28ccf4cc35c312b0d58ee75848
nikitongames <n5kqjn7fk9@privaterelay.appleid.com>
2025-10-27 23:51:23 +0500

    Create UIManager.cs

---
+++ nikiton-core/Assets/Core/UIManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Nikiton.Core
+{
+    public static class UIManager
+    {
+        public static void Initialize()
+        {
+            Debug.Log("UI system ready");
+        }
+    }
+}
