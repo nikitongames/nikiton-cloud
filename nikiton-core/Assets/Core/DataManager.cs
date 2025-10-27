2b6113c52f07cca0a5dd637d1eb9ac0f31a6d7c8
nikitongames <n5kqjn7fk9@privaterelay.appleid.com>
2025-10-27 23:50:49 +0500

    Create DataManager.cs

---
+++ nikiton-core/Assets/Core/DataManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Nikiton.Core
+{
+    public static class DataManager
+    {
+        public static void Initialize()
+        {
+            Debug.Log("Data system ready");
+        }
+    }
+}
