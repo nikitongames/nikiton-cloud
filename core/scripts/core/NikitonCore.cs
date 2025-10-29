42e1b201649633fbf888051850a11f6a339011f5
nikitongames <n5kqjn7fk9@privaterelay.appleid.com>
2025-10-29 23:13:02 +0500

    Update NikitonCore.cs

--- core/scripts/core/NikitonCore.cs
+++ core/scripts/core/NikitonCore.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class NikitonCore : MonoBehaviour
 {
@@ -16,10 +17,20 @@ public class NikitonCore : MonoBehaviour
         // 2) Экран загрузки
         yield return NikitonLoadingScreen.ShowLoading();
 
-        // 3) Обновление ядра (тихо/авто)
+        // 3) Инициализация валют/регионов/цен
+        string regions = File.ReadAllText("core/configs/system/regions.json");
+        string forex   = File.ReadAllText("updates/forex/exchange_rates.json");
+        string rules   = File.ReadAllText("core/configs/shop/pricing_rules.json");
+        Pricing.Init(regions, forex, rules);
+
+        // 4) Обновление ядра
         yield return NikitonUpdater.CheckForUpdates();
 
-        // 4) Контент по согласию игрока (локации/события/наборы)
+        // 5) Контент по согласию игрока (локации/события/наборы)
         yield return NikitonContentManager.CheckAndAskForContent();
+
+        // 6) Модули магазина и туров
+        yield return ShopSystem.Init();
+        yield return TourManager.Init();
     }
 }
