7f2cbf969e1a6e70ddab0d7f37082d5551781a83
nikitongames <n5kqjn7fk9@privaterelay.appleid.com>
2025-10-29 23:24:35 +0500

    Create TourManager.cs

---
+++ core/modules/shop/TourManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+public static class TourManager
+{
+    public static IEnumerator Init()
+    {
+        Debug.Log("[Tours] Система туров активна");
+        yield break;
+    }
+}
