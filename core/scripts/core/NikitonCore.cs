607bbf2a5471bf28dd3db5bb087c2a677117d5f7
nikitongames <n5kqjn7fk9@privaterelay.appleid.com>
2025-10-29 22:57:42 +0500

    Update NikitonCore.cs

--- core/scripts/core/NikitonCore.cs
+++ core/scripts/core/NikitonCore.cs
@@ -10,8 +10,16 @@ public class NikitonCore : MonoBehaviour
 
     IEnumerator Initialize()
     {
+        // 1) Заставка студии
         yield return NikitonSplashScreen.ShowLogo();
+
+        // 2) Экран загрузки
         yield return NikitonLoadingScreen.ShowLoading();
+
+        // 3) Обновление ядра (тихо/авто)
         yield return NikitonUpdater.CheckForUpdates();
+
+        // 4) Контент по согласию игрока (локации/события/наборы)
+        yield return NikitonContentManager.CheckAndAskForContent();
     }
 }
