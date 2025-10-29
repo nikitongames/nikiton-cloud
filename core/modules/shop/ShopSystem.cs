bb01184ef542fda2a1f15837155d6264544b5fb2
nikitongames <n5kqjn7fk9@privaterelay.appleid.com>
2025-10-29 23:24:19 +0500

    Create ShopSystem.cs

---
+++ core/modules/shop/ShopSystem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+public static class ShopSystem
+{
+    public static IEnumerator Init()
+    {
+        Debug.Log("[Shop] Магазин инициализирован");
+        yield break;
+    }
+}
